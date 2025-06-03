d69c5c119a205bb746cda86d57533d57ce4bfcf3
NEPAMPUHI <140514246+NEPAMPUHI@users.noreply.github.com>
2025-06-03 07:56:00 +0300

    added general structure

--- PO_lab3.cpp
+++ PO_lab3.cpp
@@ -1,7 +1,86 @@
 #include <iostream>
+#include <thread>
+#include <queue>
+#include <shared_mutex>
+#include <chrono>
+#include <vector>
 
 using namespace std;
 
+const int min_time_ms = 3000;
+const int max_time_ms = 6000;
+const int generator_thread_num = 2;
+const int worker_thread_num = 4;
+
+class Task {
+	static int next_id;
+	int id;
+	int delay_ms;
+
+public:
+	Task() {
+		id = ++next_id;
+		delay_ms = rand() % (max_time_ms - min_time_ms + 1) + min_time_ms;
+	}
+
+	Task(const Task&) = delete;
+
+	Task& operator=(const Task&) = delete;
+
+	void execute() {
+		this_thread::sleep_for(chrono::milliseconds(delay_ms));
+	}
+};
+
+int Task::next_id = 0;
+
+class TaskGenerator {
+	ThreadSafeQueue<Task> queue;
+	thread th;
+	atomic<bool> should_continue;
+
+public:
+	void start() {
+		while (should_continue.load()) {
+			Task t;
+
+		}
+	}
+};
+
+
+template <typename T>
+class ThreadSafeQueue {
+	queue<T> q;
+	shared_mutex m;
+
+
+
+
+};
+
+class ThreadPool {
+	vector<thread> workers;
+	ThreadSafeQueue<Task> queue;
+
+public:
+	ThreadPool() {}
+
+	void start() {}
+
+	void pause() {}
+
+	void resume() {}
+
+	void stop() {}
+
+	void complete_and_stop() {}
+
+};
+
 int main() {
+	ThreadSafeQueue<Task> queue;
+	ThreadPool pool;
+	vector<TaskGenerator> generators;
 	return 0;
 }
